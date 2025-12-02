1ae8283edec91c47411164657c7cef86a4517a22
wsh <zlwm0000@naver.com>
2025-12-02 16:13:04 +0900

    update

---
+++ DTD/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GameCanvas from "./components/GameCanvas/GameCanvas";
+
+function App() {
+  return (
+    <div>
+      <GameCanvas />
+    </div>
+  );
+}
+
+export default App;
